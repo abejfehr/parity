cba6aa82f45bd0e9789507feafdf1a538fce66db
abejfehr <abe.fehr@gmail.com>
2014-04-01 19:09:54 -0400

    initial commit

---
+++ js/parity.js
@@ -0,0 +1,17 @@
+//
+// Parity
+// Author:  Abe Fehr
+// Version: 0.1
+//
+
+var level = 0;
+/*
+
+STEPS
+=====
+* keep track of what "level" we're on
+* at the beginning of the game, show the instructions
+ (and maybe give the option to skip)
+* show the level, and maybe the level stores what instructions come before it...
+* are the levels stored as objects?
+*/
