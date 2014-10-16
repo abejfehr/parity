b910a348e8d0b848039c68f100d1ed985380c717
abejfehr <abe.fehr@gmail.com>
2014-10-16 19:19:01 -0400

    moved cookies, changed version

--- js/parity.js
+++ js/parity.js
@@ -1,7 +1,9 @@
 //
-// Parity
+// File:    parity.js
 // Author:  Abe Fehr
-// Version: 0.1
+// Version: 0.2
+//
+// Contains the main game logic for parity
 //
 
 $(document).ready(function() {
@@ -474,32 +476,6 @@ $(document).ready(function() {
     }
     return -1;
   }
-
-
-
-  //saves a cookie with the current level
-  function saveProgress() {
-    var d = new Date();
-    d.setTime(d.getTime()+(365*24*60*60*1000));
-    var expires = "expires="+d.toGMTString();
-    document.cookie = "parity_last_level=" + level.number + "; " + expires;
-  }
-
-  //gets the last level from a cookie
-  function loadProgress() {
-    var name = "parity_last_level=";
-    var cookieArray = document.cookie.split(';');
-    var levelNo = -1;
-    for(var i=0; i<cookieArray.length; ++i) {
-      var cookie = cookieArray[i].trim();
-      if (cookie.indexOf(name) == 0) {
-        levelNo = cookie.substring(name.length,cookie.length);
-      }
-    }
-
-    setLevel(levelNo);
-  }
-
 
   //event assignments
   button.click(advanceStory);
